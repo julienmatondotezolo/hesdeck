8033cf3ddf8a77410cac662d7af3415e5ad4119f
Julien Goldy <juliengoldy@Juliens-MacBook-Pro.local>
2023-07-25 09:45:22 +0200

    Creating helper function to add deck

---
+++ lib/utils/helpers.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:hessdeck/models/deck.dart';
+import 'package:hessdeck/providers/deck_provider.dart';
+import 'package:hessdeck/themes/colors.dart';
+import 'package:provider/provider.dart';
+
+class Helpers {
+  // Function to show a dialog
+  static void showAddDeckDialog(
+      BuildContext context, String message, int deckIndex) {
+    final deckNameController = TextEditingController();
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text(message),
+          content: TextField(
+            controller: deckNameController,
+            decoration: InputDecoration(labelText: message),
+          ),
+          actions: [
+            ElevatedButton(
+              onPressed: () {
+                if (deckNameController.text.isNotEmpty) {
+                  final newDeck = Deck(
+                    name: deckNameController.text,
+                    iconData: Icons.widgets,
+                    buttons: [], // Replace with the desired icon
+                  );
+                  Provider.of<DeckProvider>(context, listen: false)
+                      .addDeck(newDeck, deckIndex);
+                  Navigator.pop(context);
+                }
+              },
+              child: const Text('Add'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  LinearGradient deckGradient() {
+    return AppColors.blueToDarkGradient;
+  }
+}
