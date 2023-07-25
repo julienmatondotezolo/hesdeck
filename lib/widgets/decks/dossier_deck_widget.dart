41249629dc626bf2751bd489142bd51abc83c0bd
Julien Goldy <juliengoldy@Juliens-MacBook-Pro.local>
2023-07-25 20:38:24 +0200

    Adding dossier widget

---
+++ lib/widgets/decks/dossier_deck_widget.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:hessdeck/models/deck.dart';
+import 'package:hessdeck/themes/colors.dart';
+import 'package:hessdeck/utils/helpers.dart';
+
+class DossierDeckWidget extends StatelessWidget {
+  final Deck? deck; // Deck can be null
+  final BuildContext context; // Pass the HomeScreen's context
+  final int deckIndex; // Index of the deck in the list
+
+  const DossierDeckWidget({
+    Key? key,
+    required this.deck,
+    required this.context,
+    required this.deckIndex,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Helpers.openDeckScreen(context, deckIndex, deck!);
+      },
+      child: Container(
+        padding: const EdgeInsets.all(8.0),
+        decoration: BoxDecoration(
+          gradient: deck!.backgroundColor,
+          border: Border.all(color: AppColors.darkGrey, width: 5),
+          borderRadius: BorderRadius.circular(10.0),
+          shape: BoxShape
+              .rectangle, // Use BoxShape.rectangle to add a rounded border
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Center(
+              child: Icon(
+                deck!.iconData,
+                color: Colors.white,
+                size: 36.0,
+              ),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              deck?.name ?? "Add name to deck",
+              style: const TextStyle(color: Colors.white, fontSize: 12),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
