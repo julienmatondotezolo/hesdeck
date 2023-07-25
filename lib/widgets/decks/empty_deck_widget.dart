db5624917d736f802519b505fe4b815e9843a1f1
Julien Goldy <juliengoldy@Juliens-MacBook-Pro.local>
2023-07-25 20:01:38 +0200

    Adding empty widget

---
+++ lib/widgets/decks/empty_deck_widget.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:hessdeck/models/deck.dart';
+import 'package:hessdeck/themes/colors.dart';
+import 'package:hessdeck/utils/helpers.dart';
+
+class EmptyDeckWidget extends StatelessWidget {
+  final Deck? deck; // Deck can be null
+  final BuildContext context; // Pass the HomeScreen's context
+  final int deckIndex; // Index of the deck in the list
+
+  const EmptyDeckWidget({
+    Key? key,
+    required this.deck,
+    required this.context,
+    required this.deckIndex,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Helpers.showAddDeckDialog(context, "Add new deck", deckIndex);
+      },
+      child: Container(
+        padding: const EdgeInsets.all(8.0),
+        decoration: BoxDecoration(
+          gradient: deck!.backgroundColor,
+          border: Border.all(color: AppColors.darkGrey, width: 5),
+          borderRadius: BorderRadius.circular(10.0),
+          shape: BoxShape.rectangle,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Center(
+              child: Icon(
+                deck!.iconData,
+                color: AppColors.lightGrey,
+                size: 48.0,
+              ),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              deck!.name,
+              style: const TextStyle(color: Colors.white, fontSize: 12),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
