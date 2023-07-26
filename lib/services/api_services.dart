9cb63eafe4c4881922805cbfb0f438425274973f
Julien Goldy <juliengoldy@Juliens-MacBook-Pro.local>
2023-07-26 02:55:47 +0200

    Adding service to fetch actions

---
+++ lib/services/api_services.dart
@@ -0,0 +1,34 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:flutter/services.dart';
+
+class ApiServices {
+  static const String baseUrl =
+      'https://api.example.com'; // Replace with your API base URL
+
+  // Function to make a GET request to the API
+  static Future<dynamic> fetchData(String endpoint) async {
+    final response = await http.get(Uri.parse('$baseUrl/$endpoint'));
+
+    if (response.statusCode == 200) {
+      // If the request is successful, parse the response JSON
+      return json.decode(response.body);
+    } else {
+      // If the request fails, throw an exception with the error message
+      throw Exception('Failed to load data');
+    }
+  }
+
+  // Function to fetch data from a local JSON file
+  static Future<dynamic> fetchActions(String fileName) async {
+    try {
+      // Load the JSON data from the local file using rootBundle
+      String jsonString = await rootBundle.loadString('assets/$fileName');
+      // Parse the JSON data and return it
+      return json.decode(jsonString);
+    } catch (e) {
+      // If there's an error, throw an exception with the error message
+      throw Exception('Failed to load data');
+    }
+  }
+}
--- lib/utils/helpers.dart
+++ lib/utils/helpers.dart
@@ -1,13 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:hessdeck/models/deck.dart';
+import 'package:hessdeck/providers/deck_provider.dart';
 import 'package:hessdeck/screens/deck_screen.dart';
+import 'package:hessdeck/services/api_services.dart';
 import 'package:hessdeck/themes/colors.dart';
+import 'package:provider/provider.dart';
 
 class Helpers {
   // Function to show a dialog
   static void showAddDeckDialog(BuildContext context, int deckIndex) {
     // Get the screen height using MediaQuery
     final screenHeight = MediaQuery.of(context).size.height;
+
+    List<dynamic> data = []; // Initialize data as an empty list
+
+    // Function to fetch data from the API
+    Future<void> fetchData() async {
+      try {
+        List<dynamic> actionData = await ApiServices.fetchActions('data.json');
+        data = actionData; // Update the data list with the fetched data
+        // You can add further logic or processing here if needed
+      } catch (error) {
+        // Handle any errors that occur during the data fetching process
+        print('Error fetching data: $error');
+      }
+    }
+
+    fetchData();
 
     showModalBottomSheet(
       context: context,
@@ -44,64 +63,71 @@ class Helpers {
                     child: Text(
                       "Add actions",
                       style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold),
+                        color: Colors.white,
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
                 ),
                 SizedBox(
                   height: screenHeight * 0.05, // 5% of the screen height
                 ),
-                Container(
-                  decoration: const BoxDecoration(
-                    border: Border(
-                      top: BorderSide(color: Colors.white10, width: 2.0),
-                    ),
-                  ),
-                  child: ExpansionTile(
-                    tilePadding: const EdgeInsets.symmetric(horizontal: 26.0),
-                    backgroundColor: AppColors.primaryBlack,
-                    collapsedBackgroundColor: AppColors.darkGrey,
-                    collapsedIconColor: AppColors.white,
-                    iconColor: Colors.white,
-                    title: const Text(
-                      'OBS',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 14,
-                        fontWeight: FontWeight.bold,
+                for (var item in data)
+                  Container(
+                    decoration: const BoxDecoration(
+                      border: Border(
+                        top: BorderSide(color: Colors.white10, width: 2.0),
                       ),
                     ),
-                    children: [
-                      GestureDetector(
-                        onTap: () {
-                          customDialog(context, 'Adding "" to deck');
-                        },
-                        child: Container(
-                          decoration: const BoxDecoration(
-                            border: Border(
-                              top:
-                                  BorderSide(color: Colors.white10, width: 1.0),
-                            ),
-                          ),
-                          padding: const EdgeInsets.symmetric(
-                              vertical: 16.0, horizontal: 26.0),
-                          child: const Align(
-                            alignment: Alignment.centerLeft,
-                            child: Text(
-                              'OBS Content',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 12,
+                    child: ExpansionTile(
+                      tilePadding: const EdgeInsets.symmetric(horizontal: 26.0),
+                      backgroundColor: AppColors.primaryBlack,
+                      collapsedBackgroundColor: AppColors.darkGrey,
+                      collapsedIconColor: AppColors.white,
+                      iconColor: Colors.white,
+                      title: Text(
+                        item['actionName'],
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      children: [
+                        for (var actionDeck in item['actionDecks'])
+                          GestureDetector(
+                            onTap: () {
+                              customDialog(
+                                  context,
+                                  'Add this ${actionDeck["name"]} action.',
+                                  deckIndex,
+                                  item['actionDecks'][0]);
+                            },
+                            child: Container(
+                              decoration: const BoxDecoration(
+                                border: Border(
+                                  top: BorderSide(
+                                      color: Colors.white10, width: 1.0),
+                                ),
+                              ),
+                              padding: const EdgeInsets.symmetric(
+                                  vertical: 16.0, horizontal: 26.0),
+                              child: Align(
+                                alignment: Alignment.centerLeft,
+                                child: Text(
+                                  actionDeck['name'],
+                                  style: const TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 12,
+                                  ),
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
               ],
             ),
           ),
@@ -110,21 +136,51 @@ class Helpers {
     );
   }
 
-  static void customDialog(BuildContext context, String message) {
-    final deckNameController = TextEditingController();
+  static void customDialog(
+      BuildContext context, String message, int deckIndex, dynamic actionDeck) {
+    // print('actionDeck $actionDeck');
 
     showDialog(
       context: context,
       builder: (context) {
         return AlertDialog(
           title: Text(message),
-          content: TextField(
-            controller: deckNameController,
-            decoration: InputDecoration(labelText: message),
-          ),
+          // content: TextField(
+          //   controller: deckNameController,
+          //   decoration: InputDecoration(labelText: message),
+          // ),
           actions: [
             ElevatedButton(
               onPressed: () {
+                final newDeck = Deck.fromJson(actionDeck);
+
+                // final newDeck = Deck(
+                //   name: actionDeck['name'],
+                //   iconData: IconData(actionDeck['iconData'],
+                //       fontFamily: 'MaterialIcons'),
+                //   backgroundColor: actionDeck['backgroundColor'] != null
+                //       ? LinearGradient(
+                //           colors: (actionDeck['backgroundColor']['colors']
+                //                   as List<dynamic>)
+                //               .map<Color>((colorValue) => Color(colorValue))
+                //               .toList(),
+                //           begin: stringToAlignment(
+                //               actionDeck['backgroundColor']['begin']),
+                //           end: stringToAlignment(
+                //               actionDeck['backgroundColor']['end']),
+                //         )
+                //       : null,
+                //   iconColor: actionDeck['iconColor'] != null
+                //       ? Color(actionDeck['iconColor'])
+                //       : null,
+                //   defaultDeck: actionDeck['defaultDeck'],
+                //   dossierDeck: actionDeck['dossierDeck'],
+                //   popupDeck: actionDeck['popupDeck'],
+                //   clickableDeck: actionDeck['clickableDeck'],
+                // );
+
+                Provider.of<DeckProvider>(context, listen: false)
+                    .addDeck(newDeck, deckIndex);
                 Navigator.pop(context);
               },
               child: const Text('Confirm'),
@@ -135,17 +191,44 @@ class Helpers {
     );
   }
 
-  // static void updateDeck(BuildContext context, int deckIndex, Deck deck) {
-  //   bool deckClickToggle = deck.clicked == true ? false : true;
-  //   final deckProvider = Provider.of<DeckProvider>(context, listen: false);
-  //   final updatedDeck = deck.copyWith(
-  //     clicked: deckClickToggle, // Mark the deck as active
-  //     backgroundColor: deckClickToggle
-  //         ? AppColors.activeBlueToDarkGradient
-  //         : AppColors.blueToGreyGradient,
-  //   );
-  //   deckProvider.updateDeck(updatedDeck, deckIndex);
-  // }
+  // Helper method to convert a string to AlignmentGeometry
+  static AlignmentGeometry stringToAlignment(String alignmentString) {
+    switch (alignmentString) {
+      case 'Alignment.topLeft':
+        return Alignment.topLeft;
+      case 'Alignment.topCenter':
+        return Alignment.topCenter;
+      case 'Alignment.topRight':
+        return Alignment.topRight;
+      case 'Alignment.centerLeft':
+        return Alignment.centerLeft;
+      case 'Alignment.center':
+        return Alignment.center;
+      case 'Alignment.centerRight':
+        return Alignment.centerRight;
+      case 'Alignment.bottomLeft':
+        return Alignment.bottomLeft;
+      case 'Alignment.bottomCenter':
+        return Alignment.bottomCenter;
+      case 'Alignment.bottomRight':
+        return Alignment.bottomRight;
+      default:
+        // If the string doesn't match any of the predefined alignments,
+        // you can return a default alignment or throw an exception, depending on your use case.
+        return Alignment.center;
+    }
+  }
+
+  static void updateDeck(BuildContext context, int deckIndex, Deck deck) {
+    bool deckClickToggle = deck.clickableDeck == true ? false : true;
+    final deckProvider = Provider.of<DeckProvider>(context, listen: false);
+    final updatedDeck = deck.copyWith(
+      backgroundColor: deckClickToggle
+          ? AppColors.activeBlueToDarkGradient
+          : AppColors.blueToGreyGradient,
+    );
+    deckProvider.updateDeck(updatedDeck, deckIndex);
+  }
 
   static void openDeckScreen(BuildContext context, int deckIndex, Deck deck) {
     Navigator.push(
