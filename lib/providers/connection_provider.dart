6ca919836691a29b21e6530e78bf4cf05c205ca6
Julien Goldy <juliengoldy@Juliens-MacBook-Pro.local>
2023-09-01 22:07:55 +0200

    Adding OBSConnection object to connect and disconnect

--- lib/providers/connection_provider.dart
+++ lib/providers/connection_provider.dart
@@ -1,24 +1,29 @@
 import 'dart:async';
+import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:hessdeck/models/connection.dart';
 import 'package:obs_websocket/obs_websocket.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class ConnectionProvider extends ChangeNotifier {
-  String? _ipAddress;
-  String? _port;
-  String? _password;
   ObsWebSocket? _obsWebSocket;
   StreamController<dynamic>? _obsEventStreamController;
+  final List<Connection> _connections = [];
+  late OBSConnection _obsConnectionObject = OBSConnection(
+    ipAddress: 'test',
+    port: '4455',
+    password: 'test',
+  );
 
-  String? get ipAddress => _ipAddress;
-  String? get port => _port;
-  String? get password => _password;
   ObsWebSocket? get obsWebSocket => _obsWebSocket;
   Stream<dynamic>? get obsEventStream => _obsEventStreamController?.stream;
+  List<Connection> get connections => _connections;
+  OBSConnection get obsConnectionObject => _obsConnectionObject;
 
   ConnectionProvider() {
-    _loadSettings();
+    // _removeConnections();
+    _loadConnectionSettings();
   }
 
   void startWebSocketListener() async {
@@ -36,43 +41,82 @@ class ConnectionProvider extends ChangeNotifier {
     }
   }
 
+  void addConnection(Connection connection) {
+    // Check if a connection of the same type already exists
+    final existingConnectionIndex = _connections.indexWhere(
+      (existingConn) => existingConn.type == connection.type,
+    );
+
+    if (existingConnectionIndex != -1) {
+      // Replace the existing connection with the new connection
+      _connections[existingConnectionIndex] = connection;
+      print("Updating current connection to list");
+    } else {
+      // Add the new connection to the list
+      _connections.add(connection);
+      print("Adding new connection to list");
+    }
+
+    notifyListeners();
+  }
+
+  // Call the clearDecks() method to remove all decks from SharedPreferences
+  Future<void> _removeConnections() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.remove('connections');
+    notifyListeners();
+    print('Remove connection from SharedPreferences.');
+  }
+
   // Load previously saved connection settings from SharedPreferences
-  Future<void> _loadSettings() async {
+  Future<void> _loadConnectionSettings() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    _ipAddress = prefs.getString('obs_ip');
-    _port = prefs.getString('obs_port');
-    _password = prefs.getString('obs_password');
+    List<String>? connectionStrings = prefs.getStringList('connections');
+
+    if (connectionStrings != null) {
+      _connections.clear();
+      for (final connString in connectionStrings) {
+        // Convert JSON string to Connection object
+        final Map<String, dynamic> connJson = jsonDecode(connString);
+
+        if (connJson["type"] == 'OBS') {
+          final connectionOBS = OBSConnection.fromJson(connJson);
+          addConnection(connectionOBS);
+          _obsConnectionObject = connectionOBS;
+        }
+      }
+    }
+
     notifyListeners();
   }
 
   // Save connection settings to SharedPreferences
-  Future<void> _saveSettings() async {
+  Future<void> _saveConnectionSettings() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    prefs.setString('obs_ip', _ipAddress ?? '');
-    prefs.setString('obs_port', _port ?? '');
-    prefs.setString('obs_password', _password ?? '');
-  }
+    List<String> connectionStrings = _connections.map((conn) {
+      // Convert each Connection object to JSON string
+      return jsonEncode(conn);
+    }).toList();
 
-  // Update connection settings
-  Future<void> updateConnectionSettings(
-      String ipAddress, String port, String password) async {
-    _ipAddress = ipAddress;
-    _port = port;
-    _password = password;
-    await _saveSettings();
+    prefs.setStringList('connections', connectionStrings);
     notifyListeners();
   }
 
-  // Connect to OBS WebSocket server
-  Future<void> connectToOBS() async {
-    if (_ipAddress == null || _port == null || _password == null) {
-      throw Exception(
-          'IP address & password and port must be set before connecting.');
-    }
+  // Update connection settings
+  // Future<void> updateConnectionSettings(
+  //     String ipAddress, String port, String password) async {
+  //   _ipAddress = ipAddress;
+  //   _port = port;
+  //   _password = password;
+  //   await _saveConnectionSettings();
+  //   notifyListeners();
+  // }
 
+  // Connect to OBS WebSocket server
+  Future<void> connectToOBS(OBSConnection obsConnectionObject) async {
     _obsWebSocket = await ObsWebSocket.connect(
-      'ws://$_ipAddress:$_port',
-      password: _password,
+      'ws://${obsConnectionObject.ipAddress}:${obsConnectionObject.port}',
+      password: obsConnectionObject.password,
       fallbackEventHandler: (Event event) =>
           print('type: ${event.eventType} data: ${event.eventData}'),
     );
@@ -80,6 +124,9 @@ class ConnectionProvider extends ChangeNotifier {
     try {
       StatsResponse stats = await _obsWebSocket!.general.getStats();
       print('Connected to OBS WebSocket server.');
+      print(stats);
+      addConnection(obsConnectionObject);
+      _saveConnectionSettings();
     } catch (e) {
       print('Error connecting to OBS WebSocket server: $e');
       throw Exception('Error connecting to OBS WebSocket server.');
