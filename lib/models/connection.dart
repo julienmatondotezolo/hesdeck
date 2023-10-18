96bc1b9c9aede70645fd37b6c3f16dd9e9181e0b
Julien Goldy <juliengoldy@Juliens-MacBook-Pro.local>
2023-10-18 16:05:24 +0200

    edit connection model class

--- lib/models/connection.dart
+++ lib/models/connection.dart
@@ -33,6 +33,7 @@ class OBSConnection extends Connection {
   Map<String, dynamic> toJson() {
     return {
       'type': type,
+      'image': image,
       'ipAddress': ipAddress,
       'port': port,
       'password': password,
@@ -42,6 +43,51 @@ class OBSConnection extends Connection {
   factory OBSConnection.fromJson(Map<String, dynamic> json) {
     return OBSConnection(
       ipAddress: json['ipAddress'],
+      port: json['port'],
+      password: json['password'],
+    );
+  }
+}
+
+class TwitchConnection extends Connection {
+  final String clientId;
+  final String port;
+  final String password;
+
+  TwitchConnection({
+    required this.clientId,
+    required this.port,
+    required this.password,
+  }) : super(
+          'Twitch',
+          'https://cdn-icons-png.flaticon.com/512/2111/2111668.png',
+        );
+
+  TwitchConnection copyWith({
+    String? clientId,
+    String? port,
+    String? password,
+  }) {
+    return TwitchConnection(
+      clientId: clientId ?? this.clientId,
+      port: port ?? this.port,
+      password: password ?? this.password,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'type': type,
+      'image': image,
+      'clientId': clientId,
+      'port': port,
+      'password': password,
+    };
+  }
+
+  factory TwitchConnection.fromJson(Map<String, dynamic> json) {
+    return TwitchConnection(
+      clientId: json['clientId'],
       port: json['port'],
       password: json['password'],
     );
