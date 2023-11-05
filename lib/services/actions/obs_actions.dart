af12a0ebc03174b6c5eadb4b3be87901d1747e5a
Julien Goldy <juliengoldy@Juliens-MacBook-Pro.local>
2023-11-05 20:47:25 +0100

    refactor obs actions

--- lib/services/actions/obs_actions.dart
+++ lib/services/actions/obs_actions.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:hessdeck/providers/connection_provider.dart';
 import 'package:hessdeck/services/connections/obs_connections.dart';
 import 'package:obs_websocket/obs_websocket.dart';
 
 const changSceneMethod = 'Change scene';
+const selectSceneMethod = 'Select OBS scene';
 const startRecordMethod = 'Start record';
 const stopRecordMethod = 'Stop record';
 const startStreamMethod = 'Start stream';
@@ -17,9 +19,9 @@ class OBSMethodMetadata {
 class OBSActions {
   static Future<void> changeScenes(
     BuildContext context,
-    ObsWebSocket? obsWebSocket,
     String sceneName,
   ) async {
+    ObsWebSocket? obsWebSocket = connectionProvider(context).obsWebSocket;
     if (await OBSConnections.checkIfConnectedToObS(context, obsWebSocket)) {
       try {
         await obsWebSocket?.scenes.setCurrentProgramScene(sceneName);
@@ -32,7 +34,22 @@ class OBSActions {
     }
   }
 
-  static Future<void> startRecord(ObsWebSocket? obsWebSocket) async {
+  static Future<void> selectScenes(
+    BuildContext context,
+  ) async {
+    ObsWebSocket? obsWebSocket = connectionProvider(context).obsWebSocket;
+    if (await OBSConnections.checkIfConnectedToObS(context, obsWebSocket)) {
+      try {
+        final scenes = await obsWebSocket?.scenes.getSceneList();
+        print('[SCENES]: $scenes');
+      } catch (e) {
+        throw Exception('Error getting scene: $e');
+      }
+    }
+  }
+
+  static Future<void> startRecord(BuildContext context) async {
+    ObsWebSocket? obsWebSocket = connectionProvider(context).obsWebSocket;
     try {
       obsWebSocket?.record.startRecord();
       debugPrint('[RECORD STATUS]: ${obsWebSocket?.record.getRecordStatus()}');
@@ -43,7 +60,8 @@ class OBSActions {
     }
   }
 
-  static Future<void> stopRecord(ObsWebSocket? obsWebSocket) async {
+  static Future<void> stopRecord(BuildContext context) async {
+    ObsWebSocket? obsWebSocket = connectionProvider(context).obsWebSocket;
     try {
       obsWebSocket?.record.stopRecord();
       debugPrint(
@@ -55,7 +73,8 @@ class OBSActions {
     }
   }
 
-  static Future<void> startStream(ObsWebSocket? obsWebSocket) async {
+  static Future<void> startStream(BuildContext context) async {
+    ObsWebSocket? obsWebSocket = connectionProvider(context).obsWebSocket;
     try {
       obsWebSocket?.stream.startStream();
       debugPrint('[RECORD STATUS]: ${obsWebSocket?.record.getRecordStatus()}');
@@ -66,7 +85,8 @@ class OBSActions {
     }
   }
 
-  static Future<void> stopStream(ObsWebSocket? obsWebSocket) async {
+  static Future<void> stopStream(BuildContext context) async {
+    ObsWebSocket? obsWebSocket = connectionProvider(context).obsWebSocket;
     try {
       obsWebSocket?.stream.stopStream();
       debugPrint('[RECORD STATUS]: ${obsWebSocket?.record.getRecordStatus()}');
@@ -78,7 +98,7 @@ class OBSActions {
   }
 
   static final Map<String, OBSMethodMetadata> obsMethodMetadata = {
-    changSceneMethod: OBSMethodMetadata(['String', 'Int']),
+    changSceneMethod: OBSMethodMetadata([selectSceneMethod]),
     startRecordMethod: OBSMethodMetadata([]),
     stopRecordMethod: OBSMethodMetadata([]),
     startStreamMethod: OBSMethodMetadata([]),
@@ -90,36 +110,38 @@ class OBSActions {
   }
 }
 
-typedef OBSMethod = Future<void> Function(BuildContext, ObsWebSocket?, String);
+typedef OBSMethod = Future<void> Function(BuildContext, String);
 
 final Map<String, OBSMethod> obsMethods = {
   changSceneMethod: OBSActions.changeScenes,
-  startRecordMethod: (
+  selectSceneMethod: (
     BuildContext context,
-    ObsWebSocket? obsWebSocket,
     String sceneName,
   ) async {
-    await OBSActions.startRecord(obsWebSocket);
+    await OBSActions.selectScenes(context);
+  },
+  startRecordMethod: (
+    BuildContext context,
+    String sceneName,
+  ) async {
+    OBSActions.startRecord(context);
   },
   stopRecordMethod: (
     BuildContext context,
-    ObsWebSocket? obsWebSocket,
     String sceneName,
   ) async {
-    await OBSActions.stopRecord(obsWebSocket);
+    OBSActions.stopRecord(context);
   },
   startStreamMethod: (
     BuildContext context,
-    ObsWebSocket? obsWebSocket,
     String sceneName,
   ) async {
-    await OBSActions.startStream(obsWebSocket);
+    OBSActions.startStream(context);
   },
   stopStreamMethod: (
     BuildContext context,
-    ObsWebSocket? obsWebSocket,
     String sceneName,
   ) async {
-    await OBSActions.stopStream(obsWebSocket);
+    OBSActions.stopStream(context);
   },
 };
