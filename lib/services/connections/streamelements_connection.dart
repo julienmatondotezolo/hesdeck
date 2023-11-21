e8d110feed4bc238f496722ebf5c837ad6ce8ea3
Julien Goldy <juliengoldy@Juliens-MacBook-Pro.local>
2023-11-21 23:59:27 +0100

    Adding streamelements connection object

---
+++ lib/services/connections/streamelements_connection.dart
@@ -0,0 +1,44 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class StreamElements {
+  final String? jwtToken;
+  final String? accountID;
+  final String baseUrl = "https://api.streamelements.com/kappa/v2/";
+  Map<String, String> headers = {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer '
+  };
+
+  StreamElements(this.jwtToken, this.accountID) {
+    headers['Authorization'] = 'Bearer $jwtToken';
+  }
+
+  Future connect(String jwtToken, String accountID) async {
+    headers['Authorization'] = 'Bearer $jwtToken';
+    final Uri uri = Uri.parse('$baseUrl/users/current');
+    final response = await http.get(uri, headers: headers);
+
+    StreamElements streamElementsObject;
+
+    if (response.statusCode == 200) {
+      streamElementsObject = StreamElements(jwtToken, accountID);
+      return streamElementsObject;
+    } else {
+      throw Exception('Authentication error wrong credentials');
+    }
+  }
+
+  Future<Map<String, dynamic>> getAllOverlays() async {
+    final Uri uri =
+        Uri.parse('$baseUrl/overlays/$accountID/?search=a&type=regular');
+
+    final response = await http.get(uri, headers: headers);
+
+    if (response.statusCode == 200) {
+      return json.decode(response.body);
+    } else {
+      throw Exception('Failed to get overlays data');
+    }
+  }
+}
