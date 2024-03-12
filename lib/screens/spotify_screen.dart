768eb444ed65802a2c3f2f31914e20a8f39693a2
Julien Goldy <juliengoldy@Juliens-MacBook-Pro.local>
2024-03-12 20:15:35 +0100

    Adding spotify package

--- lib/screens/spotify_screen.dart
+++ lib/screens/spotify_screen.dart
@@ -1,96 +1,77 @@
 import 'package:flutter/material.dart';
-import 'package:spotify_sdk/spotify_sdk.dart';
+import 'package:spotify_api/spotify_api.dart';
 
 class SpotifyScreen extends StatelessWidget {
   const SpotifyScreen({super.key});
 
-  final String clientId = "821782afcd0341d2b7a75d0d808240e5";
-  final String redirectURI = "com.hessdeck.tv.hessdeck://login-callback";
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Spotify Control'),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              ElevatedButton(
-                onPressed: () => _connectToSpotify(clientId, redirectURI),
-                child: const Text('Connect to Spotify'),
+    // SpotifyApi spotifyAuth = SpotifyApi();
+    SpotifyApi spotifyAuth = SpotifyApi(
+      clientId: '821782afcd0341d2b7a75d0d808240e5',
+      clientSecret: '6bfb65fa73cf4f2bb7e6cea4841e6bc9',
+      redirectUri: 'com.hessdeck.tv.hessdeck://login-callback',
+    );
+
+    return SafeArea(
+      child: Container(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Center(
+              child: ElevatedButton(
+                onPressed: () async {
+                  await spotifyAuth.launchAuth(context);
+
+                  // List<dynamic> playlists = await spotifyAuth.getUserPlaylists();
+                  // print(playlists);
+                },
+                child: const Text('Authenticate with Spotify'),
               ),
-              ElevatedButton(
-                onPressed: () => _playerState(),
-                child: const Text('Player state'),
+            ),
+            const Spacer(),
+            const Text(
+              'Data',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20.0,
               ),
-              ElevatedButton(
-                onPressed: () => _pauseSong(),
-                child: const Text('Pause Song'),
-              ),
-              ElevatedButton(
-                onPressed: () => _resumeSong(),
-                child: const Text('Resume Song'),
-              ),
-              ElevatedButton(
-                onPressed: () => _nextSong(),
-                child: const Text('Next Song'),
-              ),
-            ],
-          ),
+            ),
+            const Spacer(),
+            ElevatedButton(
+              onPressed: () async {
+                await spotifyAuth.startPlayback();
+              },
+              child: const Text('Start playback'),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                await spotifyAuth.pausePlayback();
+              },
+              child: const Text('Pause playback'),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                await spotifyAuth.nextPlayback();
+              },
+              child: const Text('Next playback'),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                await spotifyAuth.previousPlayback();
+              },
+              child: const Text('Previous playback'),
+            ),
+            const Spacer(),
+            ElevatedButton(
+              onPressed: () async {
+                await spotifyAuth.disconnect();
+              },
+              child: const Text('Disconnect'),
+            )
+          ],
         ),
       ),
     );
-  }
-
-  void _connectToSpotify(String clientId, String redirectURI) async {
-    await SpotifySdk.connectToSpotifyRemote(
-      clientId: clientId,
-      redirectUrl: redirectURI,
-    );
-
-    final accessToken = await SpotifySdk.getAccessToken(
-        clientId: clientId,
-        redirectUrl: redirectURI,
-        scope: "streaming,user-modify-playback-state,playlist-read-privat"
-        // scope:
-        //     "app-remote-control,user-modify-playback-state,playlist-read-private",
-        );
-
-    SpotifySdk.subscribePlayerState();
-    SpotifySdk.subscribePlayerContext();
-
-    debugPrint('access token: $accessToken');
-    print('access token: $accessToken');
-  }
-
-  void _playerState() async {
-    final playerState = await SpotifySdk.getPlayerState();
-
-    debugPrint('player state: $playerState');
-    print('player state: $playerState');
-  }
-
-  void _pauseSong() async {
-    final pause = await SpotifySdk.pause();
-
-    debugPrint('pause: $pause');
-    print('pause: $pause');
-  }
-
-  void _resumeSong() async {
-    final resume = await SpotifySdk.resume();
-
-    debugPrint('resume: $resume');
-    print('resume: $resume');
-  }
-
-  void _nextSong() async {
-    final next = await SpotifySdk.skipNext();
-
-    debugPrint('next: $next');
-    print('next: $next');
   }
 }
